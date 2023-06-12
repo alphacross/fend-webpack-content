37afde929b4206544c9f8ff3e17680564a6ed3dd
Andreas Henggie <andreas.henggie@bertelsmann.de>
2023-06-12 17:56:54 +0800

    Add Clean webpack plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,15 @@
 const path = require("path")
 const webpack = require("webpack")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     mode: 'development',
-    devtool : 'source-map',
+    devtool: 'source-map',
     entry: './src/client/index.js',
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
@@ -19,6 +21,13 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/client/views/index.html',
             filename: './index.html'
-        })
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new BundleAnalyzerPlugin()
     ]
 }
